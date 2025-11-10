caac951502a5c2e33e9cddeee590cb0ec3b23f44
adjlevis <adjlevis@qq.com>
2025-11-10 15:08:53 +0800

    Update docker-build.sh

--- openwrt/docker-build.sh
+++ openwrt/docker-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-docker run --rm -it \
-  -v $(pwd)/bin:/builder/bin \
-  -v $(pwd)/files:/builder/files \
-  -v $(pwd)/build.sh:/builder/build.sh \
+docker run --rm \
+  -v "$(pwd)/bin:/builder/bin" \
+  -v "$(pwd)/files:/builder/files" \
+  -v "$(pwd)/build.sh:/builder/build.sh" \
   openwrt/imagebuilder:armsr-armv7-master /builder/build.sh
